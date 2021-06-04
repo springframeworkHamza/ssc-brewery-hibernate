664ce7925c3c75403f7a5f6679999c2acd505784
springframeworkHamza <kizotomandera@hotmail.com>
2021-06-04 15:40:13 +0200

    jpa entities

--- ssc-brewery-cust-auth-url-param-filter/src/main/java/guru/sfg/brewery/security/Authority.java
+++ ssc-brewery-cust-auth-url-param-filter/src/main/java/guru/sfg/brewery/security/Authority.java
@@ -1,4 +1,17 @@
 package guru.sfg.brewery.security;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String role;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Set<User> users;
 }
--- ssc-brewery-cust-auth-url-param-filter/src/main/java/guru/sfg/brewery/security/User.java
+++ ssc-brewery-cust-auth-url-param-filter/src/main/java/guru/sfg/brewery/security/User.java
@@ -1,15 +1,27 @@
 package guru.sfg.brewery.security;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class User {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String userName;
     private String password;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_authority",
+                joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
+                inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")})
     private Set<Authority> authorities;
-    private Boolean accountNonExpired;
-    private Boolean accountNonLocked;
-    private Boolean credentialsNonExpired;
-    private Boolean enabled;
+
+    private Boolean accountNonExpired = true;
+    private Boolean accountNonLocked = true;
+    private Boolean credentialsNonExpired = true;
+    private Boolean enabled = true;
 
 }
